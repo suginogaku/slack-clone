4d43e7b32e274f22ffdc22a9e6c9f7d7181894f6
suginogaku <g-sugino@macro-eg.co.jp>
2025-07-18 15:30:51 +0900

    ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import CreateWorkspace from "./pages/CreateWorkspace"
+import Home from "./pages/Home"
+
 function App() {
   return (
     <>
-      <div>hello world!</div>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/sign_up" element={<Signup/>}></Route>
+            <Route path="/sign_in" element={<Signin/>}></Route>
+            <Route path="/" element={<CreateWorkspace/>}></Route>
+            <Route path="/:workspaceId/:channelId" element={<Home/>}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
